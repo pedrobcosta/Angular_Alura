d56d248ffc4a9b3c24e481daff550a6e44ebdb55
F8233989 Pedro Emanuel Borges Costa <pedro.costa@bb.com.br>
2022-07-13 17:12:06 -0300

    aula 2

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+
+  valor: number = 12;
+  destino: number = 222;
+
   transferir() {
     console.log('Solicitada nova transferência');
+    console.log("Valor: " + this.valor);
+    console.log("Destino: " + this.destino);
   }
 }
